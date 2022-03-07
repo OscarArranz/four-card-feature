b9f41f7c19bc31b0e28dc826839948c1e9f837b2
Oscar Arranz <oscararranzp@gmail.com>
2022-03-07 21:53:23 +0100

    Add responsive design

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import CalculatorIcon from './assets/icon-calculator.svg';
 function App() {
   return (
     <div className="text-center py-20 px-9">
-      <header>
-        <h1 className="font-thin text-2xl text-very-dark-blue leading-9 mb-5">
+      <header className="lg:flex flex-col items-center">
+        <h1 className="font-thin text-3xl text-very-dark-blue leading-9 mb-5">
           Reliable, efficient delivery
           <br />
           <b className="font-bold">Powered by Technology</b>
         </h1>
-        <p className="text-grayish-blue tracking-tight mb-20">
+        <p className="text-grayish-blue tracking-tight mb-20 lg:w-4/12 lg:px-12">
           Our Artificial Intelligence powered tools use millions of project data
           points to ensure that your project is successful
         </p>
       </header>
-      <main>
+      <main className="block 2xl:w-7/12 2xl:px-8 lg:w-4/5 lg:m-auto lg:grid lg:grid-cols-3 lg:grid-rows-4 lg:gap-6">
         <Card
+          className="lg:row-start-2"
           title="Supervisor"
           content="Monitors activity to identify project roadblocks"
           color="cyan"
@@ -32,12 +33,14 @@ function App() {
           iconURL={TeamBuilderIcon}
         />
         <Card
+          className="lg:col-start-2"
           title="Karma"
           content="Regularly evaluates our talent to ensure quality"
           color="orange"
           iconURL={KarmaIcon}
         />
         <Card
+          className="lg:col-start-3 lg:row-start-2"
           title="Calculator"
           content="Uses data from past projects to provide better delivery estimates"
           color="blue"
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,10 @@
 // border-orange
 // border-blue
 
-export default function Card({ title, content, iconURL, color }) {
+export default function Card({ title, content, iconURL, color, className }) {
   return (
     <div
-      className={`flex flex-col items-start text-left bg-white rounded-md border-t-4 py-6 px-8 mb-6 shadow-card border-${color}`}
+      className={`flex flex-col items-start lg:row-span-2 text-left bg-white rounded-md border-t-4 py-6 px-8 mb-6 shadow-card border-${color} ${className}`}
     >
       <h2 className="text-xl font-bold text-very-dark-blue mb-2">{title}</h2>
       <p className="text-grayish-blue text-xs tracking-normal mb-8">
